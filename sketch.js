d72e9eb7e0a41b480013d53bbcb42732858fdb65
mayko084 <61643547+mayko084@users.noreply.github.com>
2023-11-11 06:52:39 +0700

    update

--- sketch.js
+++ sketch.js
@@ -3,46 +3,43 @@ let margin = 40;
 let itter = 10;
 let radius;
 let step;
-let leftTextMargin = 30; // Margin from the left in pixels for the text
+let leftTextMargin = 30;
+let userName = '';
+let userSurname = '';
 
 function setup() {
-  pixelDensity(2);
-  canvas = createCanvas(700, 900); // Scaled down A4 size with the same aspect ratio
-  canvas.parent('canvasContainer'); // Attach the canvas to the div
-  step = (width - margin * 2) / (itter - 1);
-  noLoop();
+    pixelDensity(2);
+    canvas = createCanvas(700, 900);
+    canvas.parent('canvasContainer');
+    step = (width - margin * 2) / (itter - 1);
+    noLoop();
 }
 
 function draw() {
-  background('#333333');
-  fill(255);
-  noStroke();
-  for (let y = 0; y < itter; y++) {
-    for (let x = 0; x < itter; x++) {
-      radius = random(5, 20);
-      ellipse(margin + x * step, margin + y * step, radius, radius);
+    background('#333333');
+    fill(255);
+    noStroke();
+    for (let y = 0; y < itter; y++) {
+        for (let x = 0; x < itter; x++) {
+            radius = random(5, 20);
+            ellipse(margin + x * step, margin + y * step, radius, radius);
+        }
     }
-  }
 
-  // Add your initial text here, if any
+    fill(255);
+    textSize(32);
+    textAlign(LEFT, CENTER);
+    text("SWU Open House - CS and DE Department", leftTextMargin, height - 170);
+    textSize(24);
+    text("We appreciate your visit to our booth.", leftTextMargin, height - 130);
+    text("Thank you for your interest!", leftTextMargin, height - 100);
+    text(userName + ' ' + userSurname, leftTextMargin, height - 70);
 }
 
-// Function to update the text on the canvas
-window.updateCertificateText = function(name, surname) {
-  // Clear the previous canvas
-  clear();
-  
-  // Redraw everything
-  draw();
-
-  // Set the name and surname on the canvas
-  fill(255);
-  textSize(32);
-  textAlign(LEFT, CENTER);
-  text("Certificate of Participation", leftTextMargin, height - 150);
-  textSize(24);
-  text("This is to certify that", leftTextMargin, height - 120);
-  textSize(30);
-  text(name + " " + surname, leftTextMargin, height - 90);
-  // Continue drawing any other text or graphics
+function displayArtwork() {
+    userName = document.getElementById('name').value;
+    userSurname = document.getElementById('surname').value;
+    document.getElementById('inputArea').style.display = 'none';
+    document.getElementById('canvasContainer').style.display = 'block';
+    redraw();
 }
